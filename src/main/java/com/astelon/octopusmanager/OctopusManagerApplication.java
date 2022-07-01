e009126cd56ca8c85bd9301c660c1a28a648815c
Mihai <banicamihai96@gmail.com>
2022-07-01 03:03:22 +0300

    Default config.yml is created on startup if missing

--- src/main/java/com/astelon/octopusmanager/OctopusManagerApplication.java
+++ src/main/java/com/astelon/octopusmanager/OctopusManagerApplication.java
@@ -6,11 +6,26 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 @SpringBootApplication
 @EnableConfigurationProperties({ ServerConfig.class, AppConfig.class })
 public class OctopusManagerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(OctopusManagerApplication.class, args);
+        try {
+            Path config = Path.of("config.yml");
+            if (!Files.exists(config)) {
+                System.out.println("Could not find config.yml, creating a default one.");
+                OctopusManagerApplication.class.getClassLoader().getResourceAsStream("config.yml");
+                Files.createFile(config);
+            }
+            SpringApplication.run(OctopusManagerApplication.class, args);
+        } catch (IOException e) {
+            System.out.println("Could not create config.yml. You may want to download it or create it yourself.");
+            e.printStackTrace();
+        }
     }
 }
